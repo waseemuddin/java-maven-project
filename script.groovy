d348113309cebe378f3c2d11fce140fc3e86dde1
waseemuddin <waseem.uddin@live.com>
2024-04-23 21:53:05 +0500

    grovy script updated

---
+++ script.groovy
@@ -0,0 +1,19 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "builing docker image..."
+        withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+            sh 'docker build -t waseem63/myrepo:jma-3.0 .'
+            sh "echo $PASS | docker login -u $USER --password-stdin"
+            sh 'docker push waseem63/myrepo:jma-3.0'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
