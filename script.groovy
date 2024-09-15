ed3b63f458236df63d4c23cfe806c5d7bcfce657
waseemuddin <waseem.uddin@live.com>
2024-09-15 12:45:13 +0500

    jenkins and grovvy file modified 2

--- script.groovy
+++ script.groovy
@@ -1,15 +1,15 @@
 
-def buildJar(){
+def buildJar() {
     echo '<h2>Step 01</h2>'
     echo '<h4>I am building the java application jar file .....<br> the below sh command for java maven packages are required<h2>'
     sh 'mvn package'
 }
-def builImage(){
+def builImage() {
     echo '<h2>Step 02</h2>'
     echo '<h4>Building the docker images and push to docker repository</h4>'
 }
 
-def deployApp(){
+def deployApp() {
     echo '<h2>Step 03</h2'
     echo '</h4>This is the final step where application successfully deployed and push to docker hub</h4>'
 
