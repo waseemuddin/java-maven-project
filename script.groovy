506ece4d564a205dae3f54797f149aa0257a663e
waseemuddin <waseem.uddin@live.com>
2024-09-15 12:19:35 +0500

    groovy file deploy

--- script.groovy
+++ script.groovy
@@ -8,8 +8,8 @@ def buildImage(){
     echo "Building the image...."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USER', passwordVariable: 'PASS')]){
         sh 'docker build -t waseem63/demo-app:jma-3.0 .'
-        sh "echo $PASS docker login -u $USER --password-stdin"
-        sh 'docker push waseem63/demo-app:3.0'
+        sh "echo $Pass | docker login -u $USER --password-stdin"
+        sh 'docker push waseem63/demo-app:jma-3.0'
     }
 }
 
