b1fa5620a5192526a907c9ca7834ccae97d2ecee
waseemuddin <waseem.uddin@live.com>
2024-09-15 12:15:51 +0500

    groovy file deploy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,28 @@
 
+def buildjar() {
+    echo "building the application..."
+    sh 'mvn package'
+}
+
+def buildImage(){
+    echo "Building the image...."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USER', passwordVariable: 'PASS')]){
+        sh 'docker build -t waseem63/demo-app:jma-3.0 .'
+        sh "echo $PASS docker login -u $USER --password-stdin"
+        sh 'docker push waseem63/demo-app:3.0'
+    }
+}
+
+def deplyApp(){
+
+    echo "deploying the app....."
+}
+
+return this
+
+
+
+ /*
  def buildJar() {
     echo "building the application..."
     sh 'mvn package'
@@ -18,3 +42,4 @@ def deployApp() {
 } 
 
 return this
+/*
