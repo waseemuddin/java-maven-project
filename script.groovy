8d91bc50410dbe047876734e8aa4e62f6f0e88e7
waseemuddin <waseem.uddin@live.com>
2024-09-15 12:33:51 +0500

    jenkins and grovvy file modified

--- script.groovy
+++ script.groovy
@@ -1,25 +1,19 @@
 
-def buildjar() {
-    echo "building the application..."
+//Step 01 build jar file
+def buildJar(){
+    echo '<h2>Step 01</h2>'
+    echo '<h4>I am building the java application jar file .....<br> the below sh command for java maven packages are required<h2>'
     sh 'mvn package'
 }
-
-def buildImage(){
-    echo "Building the image...."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USER', passwordVariable: 'PASS')]){
-        sh 'docker build -t waseem63/demo-app:jma-3.0 .'
-        sh "echo $Pass | docker login -u $USER --password-stdin"
-        sh 'docker push waseem63/demo-app:jma-3.0'
-    }
+// step 02 building images and login docker account and push it
+def builImage(){
+    echo '<h2>Step 02</h2>'
+    echo '<h4>Building the docker images and push to docker repository</h4>'
 }
 
-def deplyApp(){
+// step 03 final step success
+def deployApp(){
+    echo '<h2>Step 03</h2'
+    echo '</h4>This is the final step where application successfully deployed and push to docker hub</h4>'
 
-    echo "deploying the app....."
 }
-
-return this
-
-
-
- 
