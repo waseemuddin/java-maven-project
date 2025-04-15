2abdb7a8ac9951a3e3a4f2c1fdc8b8ca2781ab27
Waseemuddin Shaikh <waseem.uddin@live.com>
2025-04-15 23:46:44 +0500

    added

---
+++ jeninspractics/script.groovy
@@ -0,0 +1,18 @@
+def buildJar() {
+    echo "building the application"
+    sh 'mvn package'
+}
+
+def buildImage() {
+        echo "This is testing stage"
+        withCredentials([usernamePassword(credentialsId: 'docker-hub-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t vpccloud/demo-app:jma-3.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push vpccloud/demo-app:jma-3.0'
+}
+def deployApp() {
+    echo 'deploying the application'
+}
+
+
+return this
