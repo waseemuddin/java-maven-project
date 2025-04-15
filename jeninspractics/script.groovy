03871bc554977f9f470d2af79c8238e26e8bdc58
Waseemuddin Shaikh <waseem.uddin@live.com>
2025-04-16 00:11:43 +0500

    added

--- jeninspractics/script.groovy
+++ jeninspractics/script.groovy
@@ -9,7 +9,9 @@ def buildImage() {
         sh 'docker build -t vpccloud/demo-app:jma-3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push vpccloud/demo-app:jma-3.0'
+    }
 }
+
 def deployApp() {
     echo 'deploying the application'
 }
