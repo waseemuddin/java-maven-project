813c168d1725a05fb86e520a71df53f17efe52a7
Waseemuddin Shaikh <waseem.uddin@live.com>
2025-04-16 00:18:23 +0500

    added

--- jeninspractics/script.groovy
+++ jeninspractics/script.groovy
@@ -1,20 +1,42 @@
+// def buildJar() {
+//     echo "building the application"
+//     sh 'mvn package'
+// }
+
+// def buildImage() {
+//         echo "This is testing stage"
+//         withCredentials([usernamePassword(credentialsId: 'docker-hub-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'docker build -t vpccloud/demo-app:jma-3.0 .'
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh 'docker push vpccloud/demo-app:jma-3.0'
+//     }
+// }
+
+// def deployApp() {
+//     echo 'deploying the application'
+// }
+
+
+// return this
+
+
+
 def buildJar() {
-    echo "building the application"
+    echo "building the application..."
     sh 'mvn package'
-}
+} 
 
 def buildImage() {
-        echo "This is testing stage"
-        withCredentials([usernamePassword(credentialsId: 'docker-hub-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t vpccloud/demo-app:jma-3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push vpccloud/demo-app:jma-3.0'
     }
-}
+} 
 
 def deployApp() {
-    echo 'deploying the application'
-}
-
+    echo 'deploying the application...'
+} 
 
 return this
